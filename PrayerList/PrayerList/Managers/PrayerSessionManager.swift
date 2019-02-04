755d3334dd567d8faa0f417a71f9f75299d78d42
Devin Davies <justmedevin@gmail.com>
2019-02-04 19:41:04 +1300

    added refresh of items after loop

--- PrayerList/PrayerList/Managers/PrayerSessionManager.swift
+++ PrayerList/PrayerList/Managers/PrayerSessionManager.swift
@@ -39,18 +39,47 @@ class PrayerSessionManager {
         var subsetForPrayer = [ItemModel]()
         switch category.setType {
         case .consecutive:
+            
             subsetForPrayer = Array(availableItems.prefix(category.totalPerSet))
+            
+            if subsetForPrayer.count < category.totalPerSet {
+                let refreshedItems = resetCategory(category: category)
+                subsetForPrayer.append(contentsOf: Array(refreshedItems.prefix(category.totalPerSet - subsetForPrayer.count)))
+            }
         case .random:
-            for _ in 0...category.totalPerSet {
+            for _ in 0...min(category.totalPerSet, availableItems.count) {
                 if let randomElement = availableItems.randomElement() , let index = availableItems.firstIndex(where: {$0.uuid == randomElement.uuid}){
                     availableItems.remove(at: index)
                     subsetForPrayer.append(randomElement)
                 }
             }
+            
+            if subsetForPrayer.count < category.totalPerSet {
+                let refreshedItems = resetCategory(category: category)
+                
+                for _ in 0...min(category.totalPerSet - subsetForPrayer.count, refreshedItems.count) {
+                    if let randomElement = availableItems.randomElement() , let index = availableItems.firstIndex(where: {$0.uuid == randomElement.uuid}){
+                        availableItems.remove(at: index)
+                        subsetForPrayer.append(randomElement)
+                    }
+                }
+                subsetForPrayer.append(contentsOf: Array(refreshedItems.prefix(category.totalPerSet - subsetForPrayer.count)))
+            }
         }
         
         items[category] = subsetForPrayer
         return subsetForPrayer
+    }
+    
+    func resetCategory(category: CategoryModel) -> [ItemModel] {
+        let itemsForReset = category.items.filter({$0.completedForSet})
+        
+        for item in itemsForReset {
+            item.completedForSet = false
+            ItemInterface.saveGroup(group: item, inContext: CoreDataManager.mainContext)
+        }
+        
+        return itemsForReset
     }
     
 }
